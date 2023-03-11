a03e91a137aa2f5088c82bae17cd3ec719747bb9
FLikeSet <stelzf117@gmail.com>
2023-03-11 19:53:31 +0500

    initial commit

---
+++ readme.md
@@ -0,0 +1,32 @@
+pug
+sass
+img and svg optimization
+js
+babel
+
+  "devDependencies": { <br>
+    "@babel/core": "^7.21.0",<br>
+    "@babel/preset-env": "^7.20.2",<br>
+    "babel-loader": "^9.1.2",<br>
+    "css-loader": "^6.7.3",<br>
+    "filemanager-webpack-plugin": "^8.0.0",<br>
+    "html-webpack-plugin": "^5.5.0",<br>
+    "image-minimizer-webpack-plugin": "^3.8.1",<br>
+    "imagemin": "^8.0.1",<br>
+    "imagemin-gifsicle": "^7.0.0",<br>
+    "imagemin-jpegtran": "^7.0.0",<br>
+    "imagemin-optipng": "^8.0.0",<br>
+    "imagemin-svgo": "^10.0.1",<br>
+    "mini-css-extract-plugin": "^2.7.2",<br>
+    "postcss-loader": "^7.0.2",<br>
+    "postcss-preset-env": "^8.0.1",<br>
+    "pug": "^2.0.4",<br>
+    "pug-loader": "^2.4.0",<br>
+    "sass": "^1.58.3",<br>
+    "sass-loader": "^13.2.0",<br>
+    "style-loader": "^3.3.1",<br>
+    "svgo": "^3.0.2",<br>
+    "webpack": "^5.75.0",<br>
+    "webpack-cli": "^5.0.1",<br>
+    "webpack-dev-server": "^4.11.1"<br>
+  }
---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+const path = require("path");
+const FileManagerPlugin = require("filemanager-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "./src", "index.js"),
+  output: {
+    path: path.join(__dirname, "./dist"),
+    filename: "index.[contenthash:5].bundle.js",
+    assetModuleFilename: path.join("assets", "[name].[contenthash][ext]"),
+  },
+  resolve: {
+    alias: {
+      normalize: path.resolve(__dirname, "./node_modules/normalize.css"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.pug$/i,
+        use: "pug-loader",
+      },
+      {
+        test: /\.(scss|css)$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.svg$/i,
+        type: "asset/resource",
+        generator: {
+          filename: path.join("icons", "[name].[contenthash][ext]"),
+        },
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join("src", "index.pug"),
+    }),
+    new FileManagerPlugin({
+      events: {
+        onStart: {
+          delete: ["dist"],
+        },
+        onEnd: {
+          copy: [
+            {
+              source: path.join("src", "static"),
+              destination: "dist",
+            },
+          ],
+        },
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+  ],
+  devServer: {
+    watchFiles: path.join(__dirname, "src"),
+    port: 3000,
+  },
+  optimization: {
+    minimizer: [
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ["gifsicle", { interlaced: true }],
+              ["jpegtran", { progressive: true }],
+              ["optipng", { optimizationLevel: 5 }],
+              ["svgo", { name: "preset-default" }],
+            ],
+          },
+        },
+      }),
+    ],
+  },
+};
